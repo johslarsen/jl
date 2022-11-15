3042fec11a9586a6743199bb5b85cbc01fc00665
Johannes Larsen <mail@johslarsen.net>
2022-11-15 23:21:14 +0100

    Inline mmap as span and do not care to leave consitent state after destructor

--- include/jl.h
+++ include/jl.h
@@ -32,10 +32,7 @@ class unique_fd {
   }
 
   ~unique_fd() noexcept {
-    if (_fd >= 0) {
-      ::close(_fd);
-      _fd = -1;
-    }
+    if (_fd >= 0) ::close(_fd);
   }
 
   [[nodiscard]] int operator*() const noexcept { return _fd; }
@@ -63,21 +60,21 @@ class unique_mmap {
   /// A common mmap. The count/offset parameters are in counts of T, not bytes.
   /// @throws std::system_error with errno and errmsg if it fails.
   explicit unique_mmap(size_t count, int prot = PROT_NONE, int flags = MAP_SHARED, int fd = -1, off_t offset = 0, const std::string &errmsg = "mmap failed")
-      : _map(mmap(nullptr, count, prot, flags, fd, offset, errmsg)) {
+      : unique_mmap(nullptr, count, prot, flags, fd, offset, errmsg) {
   }
 
   /// More advanced constructor for e.g. MAP_FIXED when you need the addr
   /// parameter. The size/offset parameters are in counts of T, not bytes.
   /// @throws std::system_error with errno and errmsg if it fails.
   unique_mmap(void *addr, size_t count, int prot = PROT_NONE, int flags = MAP_SHARED, int fd = -1, off_t offset = 0, const std::string &errmsg = "mmap failed")
-      : _map(mmap(addr, count, prot, flags, fd, offset, errmsg)) {
-  }
+      : _map([&] {
+          void *pa = ::mmap(addr, count * sizeof(T), prot, flags, fd, offset * sizeof(T));
+          if (pa == MAP_FAILED) throw errno_as_error(errmsg);     // NOLINT(*cstyle-cast,*int-to-ptr)
+          return std::span<T>(reinterpret_cast<T *>(pa), count);  // NOLINT(*reinterpret-cast)
+        }()) {}
 
   ~unique_mmap() noexcept {
-    if (!_map.empty()) {
-      ::munmap(_map.data(), _map.size() * sizeof(T));
-      _map = std::span<T>{};
-    }
+    if (!_map.empty()) ::munmap(_map.data(), _map.size() * sizeof(T));
   }
 
   [[nodiscard]] T &operator[](size_t idx) noexcept { return _map[idx]; }
@@ -108,13 +105,6 @@ class unique_mmap {
     }
     return *this;
   }
-
- private:
-  static std::span<T> mmap(void *addr, size_t count, int prot, int flags, int fd, off_t offset, const std::string &errmsg) {
-    void *pa = ::mmap(addr, count * sizeof(T), prot, flags, fd, offset * sizeof(T));
-    if (pa == MAP_FAILED) throw errno_as_error(errmsg);     // NOLINT(*cstyle-cast,*int-to-ptr)
-    return std::span<T>(reinterpret_cast<T *>(pa), count);  // NOLINT(*reinterpret-cast)
-  }
 };
 
 /// A named file descriptor that is closed and removed upon destruction.
@@ -125,6 +115,7 @@ class tmpfd {
  public:
   explicit tmpfd(const std::string &prefix = "/tmp/jl_tmpfile_", const std::string &suffix = "")
       : tmpfd(prefix + "XXXXXX" + suffix, static_cast<int>(suffix.length())) {}
+
   ~tmpfd() {
     if (*_fd >= 0) std::filesystem::remove(_path);
   }
