54c9d3e4b66aa916a0ab785e8c8cf6daf8f1ae88
Johannes Larsen <mail@johslarsen.net>
2023-10-08 03:45:21 +0200

    Add missing rw_loop_test.cpp

---
+++ test/rw_loop_test.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+#include <jl.h>
+
+TEST(RwLoop, NominalRepeat) {
+  std::vector<off_t> offsets;
+  auto process_upto_10 = [&offsets](size_t remaining, off_t off) {
+    offsets.push_back(off);
+    return remaining > 10 ? 10 : remaining;
+  };
+  EXPECT_EQ(0, jl::rw_loop(process_upto_10, 0, ""));
+  EXPECT_EQ((std::vector<off_t>{}), offsets) << "never called when there was no length to process";
+
+  offsets.clear();
+  EXPECT_EQ(10, jl::rw_loop(process_upto_10, 10, ""));
+  EXPECT_EQ((std::vector<off_t>{0}), offsets) << "called once";
+
+  offsets.clear();
+  EXPECT_EQ(25, jl::rw_loop(process_upto_10, 25, ""));
+  EXPECT_EQ((std::vector<off_t>{0, 10, 20}), offsets) << "called repeatedly";
+}
+
+TEST(RwLoop, BreakOnEOF) {
+  auto eof_at_25 = [available = 25UL](size_t remaining, off_t) mutable {
+    size_t batch = remaining > 10 ? 10 : remaining;
+    if (available < remaining) batch = available;
+
+    available -= batch;
+    return batch;
+  };
+  EXPECT_EQ(25, jl::rw_loop(eof_at_25, 30, ""));
+}
+
+TEST(RwLoop, NonRetryableErrors) {
+  auto serious_error_only_on_first_attempt = [attempts = 2](size_t, off_t) mutable {
+    errno = ETIMEDOUT;
+    return --attempts > 0 ? -1 : 42;
+  };
+  EXPECT_THROW(jl::rw_loop(serious_error_only_on_first_attempt, 100, ""), std::system_error);
+}
+
+TEST(RwLoop, RetryAbleErrors) {
+  auto five_eagain = [attempts = 5](size_t, off_t) mutable {
+    errno = EAGAIN;
+    return --attempts > 0 ? -1 : 42;
+  };
+  EXPECT_EQ(84, jl::rw_loop(five_eagain, 84, ""));
+  EXPECT_THROW(jl::rw_loop(five_eagain, 84, "", 4), std::system_error);
+}
