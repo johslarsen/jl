bcc99a5acd2c4910b3f16aff6d7ccb7fdb8bc81d
Johannes Larsen <mail@johslarsen.net>
2024-06-04 01:07:14 +0200

    Test conversion from system time to utc clock

--- test/time_test.cpp
+++ test/time_test.cpp
@@ -28,17 +28,17 @@ TEST_SUITE("time") {
     }
 
     SUBCASE("conversion from UTC to TAI is consistent") {
-      CHECK("2017-01-01 00:00:35" == std::format("{}", clock_cast<tai_clock>(last_leap - 1s)));
-      CHECK("2017-01-01 00:00:36" == std::format("{}", clock_cast<tai_clock>(last_leap)));
-      CHECK("2017-01-01 00:00:37" == std::format("{}", clock_cast<tai_clock>(last_leap + 1s)));
+      CHECK("2017-01-01 00:00:35" == std::format("{}", tai_clock::from_utc(last_leap - 1s)));
+      CHECK("2017-01-01 00:00:36" == std::format("{}", tai_clock::from_utc(last_leap)));
+      CHECK("2017-01-01 00:00:37" == std::format("{}", tai_clock::from_utc(last_leap + 1s)));
     }
 
     SUBCASE("conversion from TAI to UTC is consistent") {
-      auto two_to_leap = clock_cast<tai_clock>(last_leap - 2s);
-      CHECK("2016-12-31 23:59:58" == std::format("{}", clock_cast<utc_clock>(two_to_leap)));
-      CHECK("2016-12-31 23:59:59" == std::format("{}", clock_cast<utc_clock>(two_to_leap + 1s)));
-      CHECK("2016-12-31 23:59:60" == std::format("{}", clock_cast<utc_clock>(two_to_leap + 2s)));
-      CHECK("2017-01-01 00:00:00" == std::format("{}", clock_cast<utc_clock>(two_to_leap + 3s)));
+      auto two_to_leap = tai_clock::from_utc(last_leap - 2s);
+      CHECK("2016-12-31 23:59:58" == std::format("{}", tai_clock::to_utc(two_to_leap)));
+      CHECK("2016-12-31 23:59:59" == std::format("{}", tai_clock::to_utc(two_to_leap + 1s)));
+      CHECK("2016-12-31 23:59:60" == std::format("{}", tai_clock::to_utc(two_to_leap + 2s)));
+      CHECK("2017-01-01 00:00:00" == std::format("{}", tai_clock::to_utc(two_to_leap + 3s)));
     }
 
     SUBCASE("conversion from UTC to system_clock skips the leap") {
@@ -48,6 +48,13 @@ TEST_SUITE("time") {
 
       CHECK("2016-12-31 23:59:59" == std::format("{}", utc_clock::to_sys(last_leap - 1s)));
       CHECK("2017-01-01 00:00:00" == std::format("{}", utc_clock::to_sys(last_leap - 1s) + 1s));
+    }
+
+    SUBCASE("conversion from system_clock to UTC skips the leap") {
+      auto two_to_leap = utc_clock::to_sys(last_leap - 2s);
+      CHECK("2016-12-31 23:59:58" == std::format("{}", utc_clock::from_sys(two_to_leap)));
+      CHECK("2016-12-31 23:59:59" == std::format("{}", utc_clock::from_sys(two_to_leap + 1s)));
+      CHECK("2017-01-01 00:00:00" == std::format("{}", utc_clock::from_sys(two_to_leap + 2s)));
     }
   }
 }
