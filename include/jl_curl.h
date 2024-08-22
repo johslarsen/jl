c207333ffec6b1295e4c40b596ee0989ab7dd46c
Johannes Larsen <mail@johslarsen.net>
2024-08-22 20:18:44 +0200

    Reorganize jl::curl library
    * Move everything into jl::curl namespace
    * jl::curl -> jl::curl::easy
    * jl::curlm -> jl::curl::multi
    * remove/replace curl prefix on lots of things, e.g.
        * e.g. jl::curl_get -> jl::curl::GET
    * add jl::easy::clean() to get a clean thread-local handle
        * and use this to so e.g. jl::curl::GET can use a defaulted handle

--- include/jl_curl.h
+++ include/jl_curl.h
@@ -4,10 +4,7 @@
 
 #include "jl.h"
 
-namespace jl {
-}  // namespace jl
-
-namespace jl::detail {
+namespace jl::curl::detail {
 
 class curl_error_category : public std::error_category {
  public:
@@ -24,37 +21,98 @@ class curlm_error_category : public std::error_category {
   }
 };
 
-}  // namespace jl::detail
+}  // namespace jl::curl::detail
 
-namespace jl {
+namespace jl::curl {
 
-inline const std::error_category& curl_error_category() noexcept {
+inline const std::error_category& easy_error_category() noexcept {
   static detail::curl_error_category instance;
   return instance;
 }
-inline const std::error_category& curlm_error_category() noexcept {
+inline const std::error_category& multi_error_category() noexcept {
   static detail::curlm_error_category instance;
   return instance;
 }
 
 template <class... Args>
-std::system_error make_curl_error(CURLcode status, std::format_string<Args...> fmt, Args&&... args) noexcept {
-  return {{static_cast<int>(status), curl_error_category()}, std::format(fmt, std::forward<Args>(args)...)};
+std::system_error make_easy_error(CURLcode status, std::format_string<Args...> fmt, Args&&... args) noexcept {
+  return {{static_cast<int>(status), easy_error_category()}, std::format(fmt, std::forward<Args>(args)...)};
 }
 template <class... Args>
-std::system_error make_curlm_error(CURLMcode status, std::format_string<Args...> fmt, Args&&... args) noexcept {
-  return {{static_cast<int>(status), curlm_error_category()}, std::format(fmt, std::forward<Args>(args)...)};
+std::system_error make_multi_error(CURLMcode status, std::format_string<Args...> fmt, Args&&... args) noexcept {
+  return {{static_cast<int>(status), multi_error_category()}, std::format(fmt, std::forward<Args>(args)...)};
+}
+
+/// WARN: in most cases this must outlive the curl handle it is set on
+class unique_slist {
+  std::unique_ptr<curl_slist, deleter<curl_slist_free_all>> _list = nullptr;
+
+ public:
+  curl_slist* operator*() { return _list.get(); }
+  void reset(curl_slist* p) { _list.reset(p); }
+
+  unique_slist& add(const char* str) {
+    decltype(_list) next(curl_slist_append(_list.get(), str));
+    if (!next) throw std::runtime_error("curl_slist_append(...) failed");
+    std::swap(_list, next);
+    std::ignore = next.release();
+    return *this;
+  }
+  unique_slist& add(const std::string& str) { return add(str.c_str()); }
+
+  [[nodiscard]] std::vector<std::string_view> dump() const {
+    std::vector<std::string_view> result;
+    for (const auto* p = _list.get(); p != nullptr; p = p->next) {
+      result.emplace_back(p->data);
+    }
+    return result;
+  }
+};
+
+/// C++ variant of https://curl.se/libcurl/c/CURLOPT_READFUNCTION.html
+using reader = std::function<size_t(std::span<std::byte>)>;
+inline size_t no_body(std::span<std::byte> /*ignored*/) { return 0; }
+inline reader read_from(std::span<const std::byte> body) {
+  return [body](std::span<std::byte> buffer) mutable {
+    auto copied = std::min(std::ssize(buffer), std::ssize(body));
+    std::copy(body.begin(), body.begin() + copied, buffer.begin());
+    body = body.subspan(copied);
+    return copied;
+  };
+}
+inline reader read_from(std::string_view body) {
+  return read_from(std::as_bytes(std::span(body)));
+}
+
+/// C++ variant of https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+using writer = std::function<size_t(std::string_view)>;
+// writer factories: {
+static size_t discard_body(std::string_view /*ignored*/) { return 0; }
+static writer append_to(std::string& buffer) {
+  return [&buffer](std::string_view s) {
+    buffer += s;
+    return s.size();
+  };
+}
+static writer overwrite(std::string& buffer) {
+  buffer.clear();
+  return append_to(buffer);
 }
 
 /// Wrapper around a CURL* handle.
-class curl {
+class easy {
  public:
-  curl() : _state(std::make_unique<stable_state>()), _curl(curl_easy_init()) {
+  easy() : _state(std::make_unique<stable_state>()), _curl(curl_easy_init()) {
     if (!_curl) throw std::runtime_error("curl_easy_init() failed");
     reset();
   }
 
-  curl& reset() {
+  static easy& clean() {
+    thread_local easy instance;
+    return instance.reset();
+  }
+
+  easy& reset() {
     curl_easy_reset(_curl.get());
     setopt(CURLOPT_ERRORBUFFER, _state->error.data());
     setopt(CURLOPT_WRITEFUNCTION, writefunction);
@@ -64,72 +122,13 @@ class curl {
     return *this;
   }
 
-  /// C++ variant of https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
-  using writer = std::function<size_t(std::string_view)>;
-  // writer factories: {
-  static size_t discard_body(std::string_view /*ignored*/) { return 0; }
-  static writer append_to(std::string& buffer) {
-    return [&buffer](std::string_view s) {
-      buffer += s;
-      return s.size();
-    };
-  }
-  static writer overwrite(std::string& buffer) {
-    buffer.clear();
-    return append_to(buffer);
-  }
-  // }
-
-  /// C++ variant of https://curl.se/libcurl/c/CURLOPT_READFUNCTION.html
-  using reader = std::function<size_t(std::span<std::byte>)>;
-  // reader factories: {
-  static size_t no_body(std::span<std::byte> /*ignored*/) { return 0; }
-  static reader read_from(std::span<const std::byte> body) {
-    return [body](std::span<std::byte> buffer) mutable {
-      auto copied = std::min(std::ssize(buffer), std::ssize(body));
-      std::copy(body.begin(), body.begin() + copied, buffer.begin());
-      body = body.subspan(copied);
-      return copied;
-    };
-  }
-  static reader read_from(std::string_view body) {
-    return read_from(std::as_bytes(std::span(body)));
-  }
-  // }
-
   /// Configure a request. Use e.g. `jl::curl_ok(curl.request(...))`, to run it
-  curl& request(const std::string& url, writer response, reader body = no_body) {
+  easy& request(const std::string& url, writer response, reader body = no_body) {
     setopt(CURLOPT_URL, url.c_str());
     _state->response = std::move(response);
     _state->body = std::move(body);
     return *this;
   }
-
-  /// WARN: in most cases this must outlive the curl handle it is set on
-  class unique_slist {
-    std::unique_ptr<curl_slist, deleter<curl_slist_free_all>> _list = nullptr;
-
-   public:
-    curl_slist* operator*() { return _list.get(); }
-    void reset(curl_slist* p) { _list.reset(p); }
-
-    unique_slist& add(const char* str) {
-      decltype(_list) next(curl_slist_append(_list.get(), str));
-      if (!next) throw std::runtime_error("curl_slist_append(...) failed");
-      std::swap(_list, next);
-      std::ignore = next.release();
-      return *this;
-    }
-    unique_slist& add(const std::string& str) { return add(str.c_str()); }
-
-    [[nodiscard]] std::vector<std::string_view> dump() const {
-      std::vector<std::string_view> result;
-      for (const auto* p = _list.get(); p != nullptr; p = p->next) {
-        result.emplace_back(p->data);
-      }
-      return result;
-    }
-  };
 
   void setopt(CURLoption setopt, const auto& value) {
     if (auto err = curl_easy_setopt(_curl.get(), setopt, value); err != CURLE_OK) {
@@ -158,7 +157,7 @@ class curl {
   CURL* operator*() { return _curl.get(); }
   template <class... Args>
   std::system_error error(CURLcode err, std::format_string<Args...> fmt, Args&&... args) noexcept {
-    return make_curl_error(err, "{}: {}", std::format(fmt, std::forward<Args>(args)...), _state->error.data());
+    return make_easy_error(err, "{}: {}", std::format(fmt, std::forward<Args>(args)...), _state->error.data());
   }
 
  private:
@@ -180,7 +179,7 @@ class curl {
 };
 
 /// @returns response code from running a blocking preconfigured CURL request
-[[nodiscard]] inline std::expected<long, std::system_error> curl_easy(curl& curl) {
+[[nodiscard]] inline std::expected<long, std::system_error> perform(easy& curl) {
   if (auto err = curl_easy_perform(*curl); err != CURLE_OK) {
     return std::unexpected(curl.error(err, "CURL request failed"));
   }
@@ -188,41 +187,41 @@ class curl {
 }
 
 /// @returns response code from successful blocking preconfigured CURL request or error if it is >= 400
-[[nodiscard]] inline std::expected<long, std::system_error> curl_ok(curl& curl) {
+[[nodiscard]] inline std::expected<long, std::system_error> ok(easy& curl) {
   curl.setopt(CURLOPT_FAILONERROR, 1);
-  return curl_easy(curl);
+  return perform(curl);
 }
 
 /// @returns the response body from a successful preconfigured CURL request
-[[nodiscard]] inline std::expected<std::string, std::system_error> curl_ok(curl& curl, const std::string& url, std::string_view body = "", std::string buffer = "") {
+[[nodiscard]] inline std::expected<std::string, std::system_error> ok(easy& curl, const std::string& url, std::string_view body = "", std::string buffer = "") {
   curl.setopt(CURLOPT_POSTFIELDSIZE, body.size());  // to avoid chunked transfer encoding
-  return curl_ok(curl.request(url, curl::overwrite(buffer), curl::read_from(body)))
+  return ok(curl.request(url, overwrite(buffer), read_from(body)))
       .transform([&buffer](long /*status*/) { return std::move(buffer); });
 }
 
-[[nodiscard]] inline std::expected<std::string, std::system_error> http_get(curl& curl, const std::string& url, std::string buffer = "") {
+[[nodiscard]] inline std::expected<std::string, std::system_error> GET(const std::string& url, easy& curl = easy::clean(), std::string buffer = "") {
   curl.setopt(CURLOPT_HTTPGET, 1);  // NOTE: automatically resets CURLOPT_POST/UPLOAD
-  return curl_ok(curl, url, "", std::move(buffer));
+  return ok(curl, url, "", std::move(buffer));
 }
-[[nodiscard]] inline std::expected<std::string, std::system_error> http_post(curl& curl, const std::string& url, std::string_view body, std::string buffer = "") {
+[[nodiscard]] inline std::expected<std::string, std::system_error> POST(const std::string& url, std::string_view body, easy& curl = easy::clean(), std::string buffer = "") {
   curl.setopt(CURLOPT_POST, 1);  // NOTE: automatically resets CURLOPT_HTTPGET/UPLOAD
-  return curl_ok(curl, url, body, std::move(buffer));
+  return ok(curl, url, body, std::move(buffer));
 }
-[[nodiscard]] inline std::expected<std::string, std::system_error> http_put(curl& curl, const std::string& url, std::string_view body, std::string buffer = "") {
+[[nodiscard]] inline std::expected<std::string, std::system_error> PUT(const std::string& url, std::string_view body, easy& curl = easy::clean(), std::string buffer = "") {
   curl.setopt(CURLOPT_UPLOAD, 1);  // NOTE: automatically resets CURLOPT_HTTPGET/POST
-  return curl_ok(curl, url, body, std::move(buffer));
+  return ok(curl, url, body, std::move(buffer));
 }
 
 /// Wrapper around a CURLM* handle.
-class curlm {
+class multi {
   std::unique_ptr<CURLM, deleter<curl_multi_cleanup>> _curlm;
-  std::unordered_map<CURL*, curl> _curls;  // in order to lookup from e.g. CURLMsg
+  std::unordered_map<CURL*, easy> _curls;  // in order to lookup from e.g. CURLMsg
 
  public:
-  curlm() : _curlm(curl_multi_init()) {
+  multi() : _curlm(curl_multi_init()) {
     if (!_curlm) throw std::runtime_error("curl_multi_init() failed");
   }
-  ~curlm() {
+  ~multi() {
     // https://curl.se/libcurl/c/curl_multi_cleanup.html suggests the deletion order:
     for (auto& [_, curl] : _curls) {  // Step 1, disassociate
       curl_multi_remove_handle(_curlm.get(), *curl);
@@ -231,10 +230,10 @@ class curlm {
     _curlm.reset();  // Step 3, cleanup CURLM* handle
   }
 
-  curl& handle(curl handle = {}) {
+  easy& handle(easy handle = {}) {
     auto p = *handle;
     if (auto ok = curl_multi_add_handle(_curlm.get(), p); ok != CURLM_OK) {
-      throw make_curlm_error(ok, "curl_multi_add_handle({}, {}) failed", _curlm.get(), p);
+      throw make_multi_error(ok, "curl_multi_add_handle({}, {}) failed", _curlm.get(), p);
     }
     return _curls.emplace(p, std::move(handle)).first->second;
   }
@@ -242,15 +241,15 @@ class curlm {
   std::expected<int, std::system_error> perform() {
     int active = 0;
     if (auto ok = curl_multi_perform(_curlm.get(), &active); ok != CURLM_OK) {
-      return std::unexpected(make_curlm_error(ok, "curl_multi_perform({}) failed", _curlm.get()));
+      return std::unexpected(make_multi_error(ok, "curl_multi_perform({}) failed", _curlm.get()));
     }
     return active;
   }
 
-  curlm(const curlm&) = delete;
-  curlm& operator=(const curlm&) = delete;
-  curlm(curlm&&) = default;
-  curlm& operator=(curlm&&) = default;
+  multi(const multi&) = delete;
+  multi& operator=(const multi&) = delete;
+  multi(multi&&) = default;
+  multi& operator=(multi&&) = default;
 };
 
-}  // namespace jl
+}  // namespace jl::curl
--- test/curl_test.cpp
+++ test/curl_test.cpp
@@ -5,14 +5,13 @@ const std::string url_to_this_file = std::format("file://{}", __FILE__);
 
 TEST_SUITE("curl") {
   TEST_CASE("file://...") {
-    jl::curl curl;
     SUBCASE("GET") {
-      auto content = jl::unwrap(jl::http_get(curl, url_to_this_file));
+      auto content = jl::unwrap(jl::curl::GET(url_to_this_file));
       CHECK(content.size() == std::filesystem::file_size(__FILE__));
     }
     SUBCASE("PUT") {
       jl::tmpfd tmp;
-      auto response = jl::unwrap(jl::http_put(curl, tmp.url(), "foo"));
+      auto response = jl::unwrap(jl::curl::PUT(tmp.url(), "foo"));
       CHECK(response == "");
 
       jl::fd_mmap<char> content(std::move(tmp).unlink());
@@ -21,7 +20,7 @@ TEST_SUITE("curl") {
   }
 
   TEST_CASE("curlm happy path") {
-    jl::curlm curlm;
+    jl::curl::multi curlm;
     std::string a, b;
     curlm.handle().request(url_to_this_file, jl::curl::overwrite(a));
     curlm.handle().request(url_to_this_file, jl::curl::overwrite(b));
@@ -31,36 +30,35 @@ TEST_SUITE("curl") {
   }
 
   TEST_CASE("make_curl_error") {
-    auto ok = jl::make_curl_error(CURLE_OK, "foo");
+    auto ok = jl::curl::make_easy_error(CURLE_OK, "foo");
     CHECK("foo: No error" == std::string(ok.what()));
-    CHECK(jl::curl_error_category() == ok.code().category());
+    CHECK(jl::curl::easy_error_category() == ok.code().category());
 
-    CHECK("CURLcode" == std::string(jl::curl_error_category().name()));
-    CHECK("Unknown error" == jl::curl_error_category().message(0xdeadbeef));
+    CHECK("CURLcode" == std::string(jl::curl::easy_error_category().name()));
+    CHECK("Unknown error" == jl::curl::easy_error_category().message(0xdeadbeef));
   }
   TEST_CASE("make_curlm_error") {
-    auto ok = jl::make_curlm_error(CURLM_OK, "foo");
+    auto ok = jl::curl::make_multi_error(CURLM_OK, "foo");
     CHECK("foo: No error" == std::string(ok.what()));
-    CHECK(jl::curlm_error_category() == ok.code().category());
+    CHECK(jl::curl::multi_error_category() == ok.code().category());
 
-    CHECK("CURLMcode" == std::string(jl::curlm_error_category().name()));
-    CHECK("Unknown error" == jl::curlm_error_category().message(0xdeadbeef));
+    CHECK("CURLMcode" == std::string(jl::curl::multi_error_category().name()));
+    CHECK("Unknown error" == jl::curl::multi_error_category().message(0xdeadbeef));
   }
 
   TEST_CASE("GET file:///NOT_FOUND") {
-    jl::curl curl;
-    CHECK(!jl::http_get(curl, "file:///NOT_FOUND").has_value());
+    CHECK(!jl::curl::GET("file:///NOT_FOUND").has_value());
   }
 
   TEST_CASE("options") {
-    jl::curl curl;
+    jl::curl::easy curl;
     curl.setopt(CURLOPT_TIMEOUT, 1);
 
     CHECK_THROWS_AS(curl.setopt(CURLOPT_POSTFIELDSIZE, -42L), std::system_error);
   }
 
   TEST_CASE("info") {
-    jl::curl curl;
+    jl::curl::easy curl;
     CHECK(nullptr == curl.info<char*>(CURLINFO_PRIVATE));
 
     auto list = curl.info<jl::curl::unique_slist>(CURLINFO_SSL_ENGINES);
@@ -75,7 +73,7 @@ TEST_SUITE("curl") {
     jl::curl::unique_slist headers;
     headers.add("Content-Type: text/plain").add("Connection: keep-alive");
 
-    jl::curl curl;
+    jl::curl::easy curl;
     curl.setopt(CURLOPT_HTTPHEADER, *headers);
   }
 }
